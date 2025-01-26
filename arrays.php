6298967fd491e7047c63c7090413550f9a7232a7
Olalla <olallasanchezbernad08@gmail.com>
2025-01-26 17:43:07 +0100

    Arrays

--- arrays.php
+++ arrays.php
@@ -8,10 +8,8 @@
 </head>
 <body>
     <?php
-        // El código está diseñado como una serie de ejercicios independientes, cada uno ilustrando un concepto de PHP.
-        // Los ejercicios se estructuran de manera incremental, presentando arrays, manipulación de datos y bucles.
 
-        // Ejercicio 1: Recorremos un array asociativo y mostramos sus valores en orden numérico.
+        // Recorro un array asociativo y muestro sus valores en orden numérico.
         echo "<b>Ejercicio 1 </b><br>";
         $persona1 = array("Nombre" => "Sara", "Apellido" => "Martinez", "Edad" => 23, "Ciudad" => "Barcelona");
         $counter = 1;
@@ -20,13 +18,13 @@
             $counter++;
         }
 
-        // Ejercicio 2: Iteramos sobre un array asociativo para mostrar las claves junto a sus valores.
+        // Itero sobre un array asociativo para mostrar las claves junto a sus valores.
         echo "<br><b>Ejercicio 2 </b><br>";
         foreach ($persona1 as $key => $value) {
             echo $key . ": " . $value . "<br>";
         }
 
-        // Ejercicio 3: Actualizamos un valor en el array y lo volvemos a mostrar.
+        // Actualizo un valor en el array y lo vuelvo a mostrar.
         echo "<br><b>Ejercicio 3 </b><br>";
         $persona1["Edad"] = 24;
         $counter = 1;
@@ -35,12 +33,12 @@
             $counter++;
         }
 
-        // Ejercicio 4: Eliminamos un elemento del array y mostramos la estructura resultante usando var_dump.
+        // Elimino un elemento del array y muestro la estructura resultante usando var_dump.
         echo "<br><b>Ejercicio 4 </b><br>";
         unset($persona1["Ciudad"]);
         var_dump($persona1);
 
-        // Ejercicio 5: Demostramos la conversión de una cadena en un array y la iteramos para mostrar sus elementos.
+        // Demuestro la conversión de una cadena en un array y la itero para mostrar sus elementos.
         echo "<br><br><b>Ejercicio 5 </b><br>";
         $letters = "a,b,c,d,e,f"; 
         $lettersAr = explode(",", $letters);
@@ -50,7 +48,7 @@
             $counter++;
         }
 
-        // Ejercicio 6: Trabajamos con un array de nombres, asignándoles valores decrecientes de un contador.
+        // Trabajo con un array de nombres, asignándoles valores decrecientes de un contador.
         echo "<b><br><br>Ejercicio 6 </b>";
         echo "<br>Notas de los estudiantes ";
         $students = "Marca, Isabel, Luis, Miguel, Aitor, Pepe"; 
@@ -61,7 +59,7 @@
             $counter--;
         }
 
-        // Ejercicio 7: Calculamos la media de las notas de los estudiantes y mostramos aquellos que están por encima de la media.
+        // Calculo la media de las notas de los estudiantes y muestro aquellos que están por encima de la media.
         echo "<b><br><br>Ejercicio 7 </b><br>";
         $estudiantes = array("Marca" => 8, "Isabel" => 5, "Luis" => 6, "Miguel" => 7, "Aitor" => 9, "Pepe" => 4);
         $media = 0;
@@ -78,7 +76,7 @@
             }
         }
 
-        // Ejercicio 8: Identificamos al mejor alumno según su nota, recorriendo el array para encontrar la nota máxima.
+        // Identifico al mejor alumno según su nota, recorriendo el array para encontrar la nota máxima.
         echo "<b><br><br>Ejercicio 8 </b><br>";
         $mejor_alumno = "";
         $nota_maxima = -1;
